0f76a990b6a98851d6fcb524e8652c3b2a5bbb79
emmanu-snapmint <emmanu.varghese@snapmintfin.com>
2025-07-22 09:51:26 +0530

    Update entries_controller.rb

--- app/controllers/plutus/entries_controller.rb
+++ app/controllers/plutus/entries_controller.rb
@@ -11,7 +11,7 @@ module Plutus
   #
   # @author Michael Bulat
   class EntriesController <  Plutus::ApplicationController
-    unloadable
+    # unloadable
     # @example
     #   GET /entries
     #   GET /entries.xml
